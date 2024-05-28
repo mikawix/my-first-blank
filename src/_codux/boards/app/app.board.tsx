a44ce02de688ee8b59ab1c372d6e4307daaed30c
Codux <codux@codux.com>
2024-05-28 18:19:05 +0300

    move root to board level

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -1,9 +1,14 @@
 import { createBoard } from '@wixc3/react-board';
 import App from '../../../App';
+import App_board_module from './app.board.module.scss';
 
 export default createBoard({
     name: 'App',
-    Board: () => <App />,
+    Board: () => (
+        <div className={App_board_module.container}>
+            <App />
+        </div>
+    ),
     environmentProps: {
         windowWidth: 1024,
         windowHeight: 768,
